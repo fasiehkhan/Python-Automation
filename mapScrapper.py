5d7d8316e2b4512e078ccd6fa514294aee73458a
Fasieh Khan <56199997+fasiehkhan@users.noreply.github.com>
2019-12-14 22:13:53 -0500

    Update mapScrapper.py
    added comments

--- mapScrapper.py
+++ mapScrapper.py
@@ -1,10 +1,12 @@
-
+#webbrowser module is imported to enable opening and searching in browser
+#pyperclip module is imported to enable successful and accurate copying of clipboard item
 import webbrowser, sys, pyperclip
 
+#the copied item from the clipboard is made a string type and put into variable address if it is longer than length 1
 if len(sys.argv) > 1:
     address = ' '.join(sys.argv[1:])
 else:
     address = pyperclip.paste()
 
-
+#a window is opened in the browser and the copied item is displayed on Google Maps!
 webbrowser.open('https://www.google.com/maps/place/' + address)
